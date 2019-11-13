c0745d3042da17219fc46affa3dfb2c372472a21
arixtty <arixtty@gmail.com>
2019-11-13 16:48:44 +0300

    Mobile version

--- script.js
+++ script.js
@@ -34,3 +34,46 @@ $(document).ready(function() {
     ]
   });
 });
+
+$(document).ready(function() {
+  $(".reviews__stories").slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: false,
+    cssEase: "ease-in"
+  });
+});
+
+var mobileBtn = document.querySelector(".mobile-nav__icon");
+var menu = document.querySelector(".navigation__list");
+var navForm = document.querySelector(".navigation__form");
+var headerContacts = document.querySelector(".header__contacts");
+var overlay = document.querySelector(".overlay");
+
+mobileBtn.addEventListener("click", function() {
+  if (menu.className !== "navigation__list open-flex") {
+    menu.className += " open-flex";
+    navForm.className += " open-flex";
+    headerContacts.className += " open-block";
+    overlay.className += ' open';
+  } else {
+    menu.className = "navigation__list";
+    navForm.className = "navigation__form";
+    headerContacts.className = "header__contacts";
+    overlay.className = 'overlay';
+  }
+});
+
+window.addEventListener('click', function(event) {
+	if(event.target === overlay) {
+    menu.className = "navigation__list";
+    navForm.className = "navigation__form";
+    headerContacts.className = "header__contacts";
+	    overlay.className = 'overlay';
+	}
+});
